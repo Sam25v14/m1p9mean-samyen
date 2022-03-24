591d2e0d38b95122fb676ad946a7215425903624
Andriatiana Samyen Lee <Sam25v14@gmail.com>
2022-03-24 13:28:57 +0300

    angular client side integration test

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/dist/client")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/dist/client", "index.html"));
+  });
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
