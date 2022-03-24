cd072757038c9255e347ce88ef6677ba75dd75ee
Andriatiana Samyen Lee <Sam25v14@gmail.com>
2022-03-24 15:32:30 +0300

    last setup for deployement

--- server/app.js
+++ server/app.js
@@ -9,9 +9,9 @@ var logger = require('morgan');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,13 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../dist/m1p9mean-samyen")));
 
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../dist/m1p9mean-samyen", "index.html"));
   });
-// }
+}
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
@@ -35,15 +35,15 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
